c5bad0dc0f8a4635d35d07867c6d5e65eaa487f8
Jared Siemen <siemenjm@gmail.com>
2022-09-13 13:55:25 -0400

    Update Project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+
 function Projects(props) {
-    return <h1>Projects</h1>;
+    const [projects, setProjects] = useState(null);
+
+    async function getProjectsData() {
+        const response = await fetch(props.URL + 'projects');
+        const data = await response.json();
+        setProjects(data);
+    }
+
+    useEffect(() => {
+        getProjectsData();
+    }, []);
+
+    function loaded() {
+        const allProjects = projects.map((project) => {
+            return (
+                <div>
+                    <h1>{project.name}</h1>
+                    <img src={project.image} alt={project.name} />
+                    <a href={project.git}>
+                        <button>Github</button>
+                    </a>
+                    <a href={project.live}>
+                        <button>Live Site</button>
+                    </a>
+                </div>
+            );
+        });
+
+        return allProjects;
+    }
+
+    return projects ? loaded() : <h1>Projects</h1>;
   }
   
 export default Projects;
