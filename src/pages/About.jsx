90121752fc9fb3f4341e2331ba2b091f5d5f0171
Jared Siemen <siemenjm@gmail.com>
2022-09-13 13:37:47 -0400

    Update About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,27 @@
+import { useState, useEffect } from "react";
+
 function About(props) {
-    return <h1>About</h1>;
+    const [about, setAbout] = useState(null);
+
+    async function getAboutData() {
+        const response = await fetch(props.URL + 'about');
+        const data = await response.json();
+        setAbout(data);
+    }
+
+    useEffect(() => {
+        getAboutData();
+    }, []);
+
+    function loaded() {
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    }
+
+    return about ? loaded() : <h1>Loading...</h1>;
   }
   
 export default About;
