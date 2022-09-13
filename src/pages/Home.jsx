ac1e6c49104b4ec2e8356bbccd827fed9100a25d
Jared Siemen <siemenjm@gmail.com>
2022-09-13 16:24:40 -0400

    Change text alignment on Home comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,10 @@ function Home(props) {
     return (
         <section className="home">
             <p className="huge">Hello</p>
-            <h2 className="name">I'm Jared.</h2>
-            <h2 className="desc">I design and develop custom websites.</h2>
+            <div>
+                <h2 className="name">I'm Jared.</h2>
+                <h2 className="desc">I design and develop custom websites.</h2>
+            </div>
         </section>
     );
   }
