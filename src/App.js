ae6d52b3948862fb662f0b0e6901e51273209226
Jared Siemen <siemenjm@gmail.com>
2022-09-13 12:35:18 -0400

    Import Routes instead of Switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Route, Switch } from 'react-router';
+import { Routes, Route } from 'react-router';
 
 import Home from './pages/Home';
 import About from './pages/About';
