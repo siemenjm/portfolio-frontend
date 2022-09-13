a6ec51df16bdcaae93e7d2eb55f7eb736871656f
Jared Siemen <siemenjm@gmail.com>
2022-09-13 13:41:41 -0400

    Fix loaded function by adding return statement

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,11 +14,13 @@ function About(props) {
     }, []);
 
     function loaded() {
-        <div>
-            <h2>{about.name}</h2>
-            <h3>{about.email}</h3>
-            <p>{about.bio}</p>
-        </div>
+        return (
+            <div>
+                <h2>{about.name}</h2>
+                <h3>{about.email}</h3>
+                <p>{about.bio}</p>
+            </div>
+        );
     }
 
     return about ? loaded() : <h1>Loading...</h1>;
