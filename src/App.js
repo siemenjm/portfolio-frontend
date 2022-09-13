79af5bcd8daf1aaf02c453ca1d3af0678a034fcb
Jared Siemen <siemenjm@gmail.com>
2022-09-13 16:59:47 -0400

    Start styling about page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import { Routes, Route } from 'react-router';
 
 import Home from './pages/Home';
-import About from './pages/About';
+import AboutMe from './pages/AboutMe';
 import Projects from './pages/Projects';
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
             <Routes>
                 <Route exact path='/' element={ <Home /> }></Route>
                 <Route path='/projects' element={ <Projects URL={URL}/> }></Route>
-                <Route path='/about' element={ <About URL={URL}/> }></Route>
+                <Route path='/about-me' element={ <AboutMe URL={URL}/> }></Route>
             </Routes>
             <Footer />
         </div>
